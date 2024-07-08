153e0f4c3204db8c9b771921cb23e1c5982ec51d
Jedrzej Adamski <jed.adamski@gmail.com>
2024-07-08 10:50:37 +0200

    output.tf fqdn added ip rm

--- aks_basic/outputs.tf
+++ aks_basic/outputs.tf
@@ -3,6 +3,10 @@ output "kube_config" {
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
 
-output "aks_ip_address" {
-  value = azurerm_kubernetes_cluster.aks.kubernetes_network_profile.load_balancer_profile.effective_outbound_ips[0].ip_address
+# output "aks_ip_address" {
+#   value = azurerm_kubernetes_cluster.aks.kubernetes_network_profile.load_balancer_profile.effective_outbound_ips[0].ip_address
+# }
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
 }
