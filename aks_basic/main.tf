fd7e4d8e11fa579a5c0c9459841250f5294fa981
Jedrzej Adamski <jed.adamski@gmail.com>
2024-07-08 08:47:29 +0200

    output.tf ip added

--- aks_basic/main.tf
+++ aks_basic/main.tf
@@ -32,6 +32,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.aks.kube_config_raw
+# }
--- aks_basic/outputs.tf
+++ aks_basic/outputs.tf
@@ -1,4 +1,8 @@
-# output "kube_config" {
-#   description = "Kube config file content"
-#   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-# }
+output "kube_config" {
+  description = "Kube config file content"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
+
+output "aks_ip_address" {
+  value = azurerm_kubernetes_cluster.aks.kubernetes_network_profile.load_balancer_profile.effective_outbound_ips[0].ip_address
+}
