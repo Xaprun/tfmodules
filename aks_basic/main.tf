e19f4102f5ce6d302a60fac65fcbc47ecdb92f97
Jedrzej Adamski <jed.adamski@gmail.com>
2024-07-08 12:45:10 +0200

    sku ip fix

--- aks_basic/main.tf
+++ aks_basic/main.tf
@@ -23,7 +23,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     load_balancer_sku = "standard"
   }
 
-
   tags = {
     Environment = "Development"
   }
@@ -35,7 +34,18 @@ resource "azurerm_public_ip" "example" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
 
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
 
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_public_ip" {
+  value = azurerm_public_ip.example[0].ip_address
+}
