f4f5daf05de1e00f90ab8e8a677d0b1a464d99d9
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-24 22:00:38 +0100

    Update main.tf

--- aks/aks_spot/main.tf
+++ aks/aks_spot/main.tf
@@ -81,4 +81,7 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
+  depends_on = [
+    azurerm_resource_group.aks_rg
+  ]
 }
