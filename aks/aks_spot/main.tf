5905a7d9ae38f001d2f5f811182ffd806607a838
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-28 13:33:12 +0100

    Update main.tf

--- aks/aks_spot/main.tf
+++ aks/aks_spot/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_resource_group" "aks_rg" {
 ###################################
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = "aks-vnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = "10.0.0.0/16"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
