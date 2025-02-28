3cbbb02645565ef4508442f56a050b8cd2109fa5
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-28 11:00:00 +0100

    Update main.tf

--- aks/aks_spot/main.tf
+++ aks/aks_spot/main.tf
@@ -60,9 +60,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
     authorized_ip_ranges = var.aks_cluster_authorized_ip   # Upewnij się, że zmienna jest listą adresów CIDR
   }
 
-  monitoring_addon {
-    enabled                    = true
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  addon_profile {
+    kube_dashboard {
+      enabled = false
+    }
+
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+    }
   }
 
   depends_on = [
