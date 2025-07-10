4da333885ab0f94f05b5aef42a3ebac8654ca63c
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 16:53:32 +0200

    Update main.tf[skip ci]

--- modules/terraform_state_bucket/main.tf
+++ modules/terraform_state_bucket/main.tf
@@ -1,9 +1,15 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstateenv${var.environment}"
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = "tfstateenv${var.environment}"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
 
   blob_properties {
     versioning_enabled = true
@@ -17,4 +23,18 @@ resource "azurerm_storage_account" "tfstate" {
     environment = var.environment
     purpose     = "terraform-state"
   }
+depends_on = [azurerm_resource_group.rg]
+}
+
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = var.container_access_type
+}
+
+resource "azurerm_storage_container" "log_container" {
+  name                  = var.log_container_name
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = var.container_access_type
 }
