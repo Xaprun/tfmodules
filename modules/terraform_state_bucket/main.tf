40aed6922ddac6afb1e18ec5b36772940444b574
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 16:32:12 +0200

    Update main.tf

--- modules/terraform_state_bucket/main.tf
+++ modules/terraform_state_bucket/main.tf
@@ -1,33 +1,21 @@
-resource "azurerm_storage_account" "tfstate" {
-  name                            = "tfstateenv${var.environment}"
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  account_tier                    = "Standard"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
-  min_tls_version                 = "TLS1_2"
-  shared_access_key_enabled       = false
-  default_to_oauth_authentication = true
-  is_hns_enabled                  = true
-  infrastructure_encryption_enabled = true
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-  tags = {
-    environment = var.environment
-    purpose     = "terraform-state"
+  blob_properties {
+    delete_retention_policy {
+      days = 90
+    }
+
+    restore_policy {
+      days = 7
+    }
+
+    container_delete_retention_policy {
+      days = 90
+    }
   }
-}
-
-resource "azurerm_storage_account_blob_properties" "default" {
-  storage_account_id = azurerm_storage_account.tfstate.id
-
-  versioning_enabled = true
-  delete_retention_policy {
-    days = 90
-  }
-}
-
-resource "azurerm_storage_account_blob_restore_policy" "default" {
-  storage_account_id = azurerm_storage_account.tfstate.id
-  enabled            = true
-  days               = 90
 }
