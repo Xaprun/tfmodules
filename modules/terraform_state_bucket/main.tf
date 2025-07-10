6502265174c90d49470217f10890d5b7aa4de799
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 16:39:49 +0200

    Update main.tf

--- modules/terraform_state_bucket/main.tf
+++ modules/terraform_state_bucket/main.tf
@@ -1,21 +1,20 @@
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = var.resource_group_name
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstateenv${var.environment}"
   location                 = var.location
+  resource_group_name      = var.resource_group_name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   blob_properties {
+    versioning_enabled = true
+
     delete_retention_policy {
       days = 90
     }
+  }
 
-    restore_policy {
-      days = 7
-    }
-
-    container_delete_retention_policy {
-      days = 90
-    }
+  tags = {
+    environment = var.environment
+    purpose     = "terraform-state"
   }
 }
