0cd7f32e49e835244e15168ac41af75a5885e125
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 12:36:14 +0200

    feat: Update main.tf improvements set

--- modules/terraform_state_bucket/main.tf
+++ modules/terraform_state_bucket/main.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_storage_account" "tfstate" {
   name                            = "tfstateenv${var.environment}"
   resource_group_name             = var.resource_group_name
@@ -10,24 +5,29 @@ resource "azurerm_storage_account" "tfstate" {
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_2"
+  shared_access_key_enabled       = false
+  default_to_oauth_authentication = true
+  is_hns_enabled                  = true
+  infrastructure_encryption_enabled = true
 
   tags = {
     environment = var.environment
     purpose     = "terraform-state"
   }
-  depends_on = [azurerm_resource_group.rg]
 }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = var.container_access_type
+resource "azurerm_storage_account_blob_properties" "default" {
+  storage_account_id = azurerm_storage_account.tfstate.id
+
+  versioning_enabled = true
+  delete_retention_policy {
+    days = 90
+  }
 }
 
-resource "azurerm_storage_container" "log_container" {
-  name                  = var.log_container_name
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = var.container_access_type
+resource "azurerm_storage_account_blob_restore_policy" "default" {
+  storage_account_id = azurerm_storage_account.tfstate.id
+  enabled            = true
+  days               = 90
 }
-
-
