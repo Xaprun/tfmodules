2717d5848ec55ab4fa54438e4d1ffe6747dcc732
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-11 06:13:48 +0200

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,9 +1,14 @@
-output "private_ips" {
-  description = "The private IP addresses of the created virtual machines"
-  value       = { for vm in azurerm_linux_virtual_machine.vm : vm.name => vm.private_ip_address }
+#output "private_ips" {
+#  description = "The private IP addresses of the created virtual machines"
+#  value       = { for vm in azurerm_linux_virtual_machine.vm : vm.name => vm.private_ip_address }
+#}
+#
+#output "public_ips" {
+#  description = "The public IP addresses of the created virtual machines"
+#  value       = { for vm in azurerm_linux_virtual_machine.vm : vm.name => azurerm_public_ip.public_ip[vm.name].ip_address }
+#}
+
+output "public_ip_addresses" {
+  value = { for k, pip in azurerm_public_ip.public_ip : k => pip.ip_address }
 }
 
-output "public_ips" {
-  description = "The public IP addresses of the created virtual machines"
-  value       = { for vm in azurerm_linux_virtual_machine.vm : vm.name => azurerm_public_ip.public_ip[vm.name].ip_address }
-}
