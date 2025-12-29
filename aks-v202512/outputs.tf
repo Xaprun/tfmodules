2874f47499e4ed5746a3c34e812befdc5101cbc1
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-12-29 22:06:22 +0100

    Update outputs.tf

--- aks-v202512/outputs.tf
+++ aks-v202512/outputs.tf
@@ -1,16 +1,19 @@
-output "id" {
-  value = azurerm_kubernetes_cluster.this.id
+output "aks_name" {
+  description = "AKS cluster name"
+  value       = azurerm_kubernetes_cluster.this.name
 }
 
-output "name" {
-  value = azurerm_kubernetes_cluster.this.name
+output "aks_fqdn" {
+  description = "AKS FQDN"
+  value       = azurerm_kubernetes_cluster.this.fqdn
 }
 
-output "fqdn" {
-  value = azurerm_kubernetes_cluster.this.fqdn
+output "aks_id" {
+  description = "AKS resource ID"
+  value       = azurerm_kubernetes_cluster.this.id
 }
 
-output "kube_config_raw" {
-  value     = azurerm_kubernetes_cluster.this.kube_config_raw
-  sensitive = true
+output "node_resource_group" {
+  description = "AKS node resource group"
+  value       = azurerm_kubernetes_cluster.this.node_resource_group
 }
