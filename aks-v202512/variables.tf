dfbb3ab8c580d87dba1273120e9ccb97911e51f0
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-12-29 20:59:17 +0100

    Update variables.tf [skip ci]

--- aks-v202512/variables.tf
+++ aks-v202512/variables.tf
@@ -201,12 +201,17 @@ variable "additional_pool_spot_max_price" {
   description = "Spot max price (-1 = on-demand price)"
 }
 
-
-  
-
 # fix ,  default = true -> false, validation added
 variable "local_account_disabled" {
   type    = bool
   default = false
   description = "Wyłącza local accounts na AKS (zalecane gdy masz AAD RBAC)"
+}
+
+# fix: tworzenie automatyczne LAW dla container insght
+
+variable "create_log_analytics" {
+  description = "Whether to create Log Analytics Workspace and Container Insights"
+  type        = bool
+  default     = true
 }
