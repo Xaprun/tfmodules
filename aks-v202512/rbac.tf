dbd0973f6938e968ef5ef564cc13d0bf924fcedd
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-12-30 11:48:00 +0100

    Update rbac.tf

--- aks-v202512/rbac.tf
+++ aks-v202512/rbac.tf
@@ -1,12 +1,13 @@
-resource "azurerm_role_assignment" "aks_ci_writer" {
-  scope                = azurerm_kubernetes_cluster.this.id
-  role_definition_name = "Azure Kubernetes Service RBAC Writer"
-  principal_id         = var.aks_ci_sp_object_id
+# odkomentuj moduł, kiedy SP uzyska prawo do nadawania ról
+# resource "azurerm_role_assignment" "aks_ci_writer" {
+#  scope                = azurerm_kubernetes_cluster.this.id
+#  role_definition_name = "Azure Kubernetes Service RBAC Writer"
+#  principal_id         = var.aks_ci_sp_object_id
 
-  depends_on = [
-    azurerm_kubernetes_cluster.this
-  ]
-}
+#  depends_on = [
+#    azurerm_kubernetes_cluster.this
+#  ]
+#}
 
 variable "aks_ci_sp_object_id" {
   description = "Object ID of Service Principal used by CI/CD"
